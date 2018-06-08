03ec17460e4a6a621aee77dcf00c047da2bf2dc9
Seamus Connolly <sghconnolly@gmail.com>
2018-06-08 12:58:05 +0100

    style: Align comments

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class BooksApp extends React.Component {
     state = {
         books: [],
         /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+         * TODO: Instead of using this state variable to keep track of which page
+         * we're on, use the URL in the browser's address bar. This will ensure that
+         * users can use the browser's back and forward buttons to navigate between
+         * pages, as well as provide a good URL they can bookmark and share.
+         */
         showSearchPage: false
     }
 
@@ -30,13 +30,13 @@ class BooksApp extends React.Component {
                             <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
                             <div className="search-books-input-wrapper">
                                 {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+                                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                                  You can find these search terms here:
+                                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */
+                                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                                  you don't find a specific author or title. Every search is limited by search terms.
+                                */
                                 }
                                 <input type="text" placeholder="Search by title or author"/>
 
