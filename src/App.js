5a03a5440b096d76ecd95bb40ba8312f63a427ab
Seamus Connolly <sghconnolly@gmail.com>
2018-06-08 14:43:02 +0100

    feat: Add shelves JSX to BookCase

--- src/App.js
+++ src/App.js
@@ -53,35 +53,7 @@ class BooksApp extends React.Component {
                         </div>
                         <div className="list-books-content">
                             <div>
-                                <div className="bookshelf">
-                                    <h2 className="bookshelf-title">Currently Reading</h2>
-                                    <div className="bookshelf-books">
-                                        <ol className="books-grid">
-                                            <li>
-                                                <div className="book">
-                                                    <div className="book-top">
-                                                        <div className="book-cover" style={{
-                                                                width: 128,
-                                                                height: 188,
-                                                                backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")'
-                                                            }}></div>
-                                                        <div className="book-shelf-changer">
-                                                            <select>
-                                                                <option value="none" disabled="disabled">Move to...</option>
-                                                                <option value="currentlyReading">Currently Reading</option>
-                                                                <option value="wantToRead">Want to Read</option>
-                                                                <option value="read">Read</option>
-                                                                <option value="none">None</option>
-                                                            </select>
-                                                        </div>
-                                                    </div>
-                                                    <div className="book-title">To Kill a Mockingbird</div>
-                                                    <div className="book-authors">Harper Lee</div>
-                                                </div>
-                                            </li>
-                                        </ol>
-                                    </div>
-                                </div>
+                                <BookCase />
                                 <BookCase />
                                 <BookCase />
                             </div>
