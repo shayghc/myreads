ede0e1d6092186ce1970c7f3eefd5af209d80e58
Seamus Connolly <sghconnolly@gmail.com>
2018-06-14 13:32:28 +0100

    feat: Display book thumbnail and title

--- src/BookSearch.js
+++ src/BookSearch.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import ShelfSelector from './ShelfSelector'
 
 class BookSearch extends React.Component {
     state = {
@@ -21,6 +22,7 @@ class BookSearch extends React.Component {
     }
 
     render() {
+        console.log(this.state.searchedBooks)
         return (<div className="search-books">
             <div className="search-books-bar">
                 <Link exact="exact" to='/' className="close-search">Close</Link>
@@ -29,7 +31,26 @@ class BookSearch extends React.Component {
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"></ol>
+                <ol className="books-grid">
+                    { this.state.searchedBooks.map((book) => (
+                        <li key={book.id}>
+                            <div className="book">
+                                <div className="book-top">
+                                    <div className="book-cover" style={{
+                                            width: 128,
+                                            height: 188,
+                                            backgroundImage: `url(${
+                                            book.imageLinks.thumbnail})`
+                                        }}/>
+                                </div>
+                                <div className="book-title">
+                                    {book.title}
+                                </div>
+                                
+                            </div>
+                        </li>
+                    ))}
+                </ol>
             </div>
         </div>)
     }
