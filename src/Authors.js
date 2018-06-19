0cfa08ed4bda4fd54671ed8d7a942317c2d54746
Seamus Connolly <sghconnolly@gmail.com>
2018-06-19 16:08:24 +0100

    chore: Add comments

--- src/Authors.js
+++ src/Authors.js
@@ -1,8 +1,15 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class Authors extends React.Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired
+    };
+
     render() {
         const { book } = this.props;
+        // Displays multiple authors on their own line
+        // Provides a no author text if book.authors is empty
         if (book.authors) {
             return (
                 <span>
