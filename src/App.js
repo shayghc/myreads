8688dffcb136b9e2ad94df5d6407d86c20fb567b
Seamus Connolly <sghconnolly@gmail.com>
2018-06-19 16:03:02 +0100

    chore: Add comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+/*
+ * @fileOverview React JS for MyReads project of the Udacity FEWD Nanodegree course.
+ * @author <a href+"mailto:sghconnolly@gmail.com">Seamus Connolly</a>
+ *
+ * TODO:
+ * Bugs:
+ * Rapid deletion of the search bar does not clear the books from the search page
+ *
+ */
+
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import BookCase from "./BookCase";
@@ -5,6 +15,10 @@ import BookSearch from "./BookSearch";
 import { Route } from "react-router-dom";
 import "./App.css";
 
+/** BooksApp class
+ * @class [BooksApp]
+ * @description This function is the main file for displaying the various states for the app
+ */
 class BooksApp extends React.Component {
     state = {
         // Holds the books already in the bookcase
@@ -33,6 +47,12 @@ class BooksApp extends React.Component {
         });
     }
 
+    /** shelfSelect class
+     * @function [shelfSelect]
+     * @description This function is passed to other components to allow the 'shelf' state to be modified
+     * @param {string} bookToUpdate. This parameter identifies the book whose shelf value is to be modified
+     * @param {string} shelf. This parameter identifies the shelf name to be assigned
+     */
     shelfSelect = (bookToUpdate, shelf) => {
         BooksAPI.update(bookToUpdate, shelf).then(response => {
             // Update the shelf for this book
